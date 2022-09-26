ae9b283589a571d687e12b550941e8dce484fbb6
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2022-09-26 13:45:48 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassLibrary1;
+
+Console.WriteLine("Hello, World!");
+
+Class1 class1 = new Class1();
+
+Console.WriteLine(class1.MyName("Martin"));
